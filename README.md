df693de705fbe6d60c9b56b645493479b703fd82
SaieshShetty <Saieshshetty710@gmail.com>
2024-10-04 12:55:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image_Cards
+ I have created a WebApp using HTML and Tailwind CSS for Frontend and Nodejs and Expressjs for Backend which creates cards for your input which takes in name ,and image url .!
---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const usermodel = require('./models/usermodel');
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/', (req,res) => {
+    res.render("index")
+})
+
+app.get('/read', async (req,res) => {
+    let users = await usermodel.find()
+    res.render("read" , {users})
+})
+
+app.get('/delete/:id', async (req,res) => {
+    let users = await usermodel.findOneAndDelete({_id: req.params.id})
+    res.redirect('/read')
+})
+
+app.get('/edit/:userid', async (req,res) => {
+    let user = await usermodel.findOne({_id: req.params.userid})
+    res.render("edit",{user})
+})
+
+app.post('/update/:userid', async (req,res) => {
+    let {image,name,email} = req.body ;
+    let updateduser = await usermodel.findOneAndUpdate({_id: req.params.userid} , {name,image,email} , {new:true})
+    res.redirect('/read')
+})
+
+app.post('/create', async (req,res) => {
+    let createduser =await usermodel.create({
+        name:req.body.name,
+        email:req.body.email,
+        image:req.body.image
+    })
+    res.redirect('/read');
+}) 
+
+app.listen(3000) ;
